d4e487d0280a07a3cf0cae97919565f02ed1b154
Began <beganbajrami@gmail.com>
2024-11-21 13:18:41 +0100

    refactoring and added 'dirln'

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 # path="$HOME/.local/bin/bscripts/"
 # mkdir -pv $path
-path="$HOME/.local/bin/"
+bin_path="$HOME/.local/bin/"
+local_path="$HOME/.local/share/linux_scripts/"
+config_path="$HOME/.config/linux_scripts/"
 
+mkdir -pv $bin_path
+mkdir -pv $config_path
+mkdir -pv $local_path
+
+cd configs
+configs=(
+	'./envs'
+)
+for config in "${configs[@]}"
+do
+    cp $config $config_path
+    echo "$config copied into "
+done
+cd ..
+
+cd scripts
 scripts=(
     "./histrm"
     "./pdfsplit"
@@ -13,10 +31,11 @@ scripts=(
     "./avenv"
     "./nospaces"
     "./myip"
+    "./dirln"
 )
 
 for script in "${scripts[@]}"
 do
-    cp $script $path
-    echo "installed $script"
+    cp $script $bin_path
+    echo "$script installed"
 done
